dd18391818c48632221dede52a5f7668532412f7
taeilyoon <zkrxpdlf0808@gmail.com>
2022-03-08 16:40:05 +0900

    build first

--- index.ts
+++ index.ts
@@ -7,12 +7,13 @@ import { type } from "os";
 import { firestore } from "@google-cloud/firestore/types/protos/firestore_v1_proto_api";
 import { build } from "protobufjs";
 import { count } from "console";
+import YAML from 'yaml';
 
-type firebaseType = "string" | "number" | "map";
+type firebaseType = "string" | "number" | "map" ;
 interface DummyDocumentData {
   name: string;
   type: firebaseType;
-  ref: string;
+  ref?: string;
   dummy: string;
 }
 interface DummyCollection {
@@ -23,6 +24,7 @@ interface DummyCollection {
 
 async function main() {
   const app = await initializeApp(data["firebase-config"]);
+  console.log('app', app);
   const parsed = data.models.map(
     (e): DummyCollection => ({
       name: e.name,
@@ -36,50 +38,65 @@ async function main() {
       ),
     })
   );
-
     faker.locale = 'ko'
     
-    const created = {
+    const uuid = {
         
     } 
 
     parsed.forEach((e) => { 
-        created[e.name] = Array(10).map((index) =>  
-            faker.datatype.uuid()
-        );
-    })
+        uuid[e.name] = Array.from(Array(10).keys()).map((index) =>(faker.datatype.uuid()));
+
+      })
+
+
     const db = getFirestore();
 
-
-    parsed.map((e) => { 
+    parsed.forEach((e, index) => { 
+      Array.from(Array(10).keys()).forEach((index)=>{
         const data = {};
         
-        e.data.map((d) => { 
-            data[`${d.name}`] = buildDUmmy(d)
+        e.data.forEach((d) => { 
+            data[`${d.name}`] = d.ref ? uuid[d.ref][index]:buildDummy(d)
         })
-        // db.collection(e.name).doc
-    })
+        
+        writeFirestoreTest(e.name, uuid[e.name][index], data)
+  
+      })
+   })
 }
 
-function writeFirestore() {}
+function writeFirestoreTest(colllection, id, data) {
+  const doc = new YAML.Document();
+  doc.contents = {
+      [`${colllection}/${id}`] : data
+    };
+  fs.appendFileSync('test.txt', doc.toString());
+}
+
+function writeFirestore({db, colllection, id}): void {
+  
+}
 
 async function initializeFirebase() {
   return await initializeApp(data["firebase-config"]);
 }
-
-main();
-function buildDUmmy(d: DummyDocumentData) {
-    return d.dummy === "name"
-      ? faker.name.firstName() + faker.name.lastName()
-      : d.dummy === "location"
-      ? new GeoPoint(+faker.address.latitude(), +faker.address.longitude())
-            : d.dummy === "date" ? Timestamp.fromMillis(faker.date.past().getMilliseconds())
-                : d.dummy === "image" ? faker.image.cats() 
-                : d.dummy === "nick" ? faker.internet.userName()    
-                        : d.dummy === "email" ? faker.internet.email()
-                        : d.dummy === "product" ? faker.commerce.productName()
-                        : d.dummy === "email" ? faker.internet.email()
-    
-            
+function buildDummy(d: DummyDocumentData) {
+  return d.dummy === "name"
+    ? faker.name.firstName() + faker.name.lastName()
+    : d.dummy === "location"
+    ? new GeoPoint(+faker.address.latitude(), +faker.address.longitude())
+          : d.dummy === "date" ? Timestamp.fromMillis(faker.date.past().getMilliseconds())
+              : d.dummy === "image" ? faker.image.cats() 
+              : d.dummy === "nick" ? faker.internet.userName()    
+                      : d.dummy === "email" ? faker.internet.email()
+                      : d.dummy === "product" ? faker.commerce.productName()
+                      : d.dummy === "email" ? faker.internet.email() 
+                      : d.dummy === "word" ? faker.word.verb()
+                      : d.dummy === "rorem" ? faker.lorem.words()
+                      : ""
 }
 
+
+
+main();
